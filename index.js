f345c946bdcabb818688ce700780e9a12c843f7c
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-02-02 14:04:29 -0300

    Config

---
+++ index.js
@@ -0,0 +1,33 @@
+//Carregando módulos
+const express = require('express')
+const handlebars = require('express-handlebars')
+const moment = require('moment')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
+const app = express()
+//Configurações
+    //Sessão
+    app.use(session({
+        secret: "@Kn5c42p6239avkssocial",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {maxAge: 24 * 60 * 60 * 1000}
+    }))
+    //Mongoose
+    mongoose.set('strictQuery', true)
+        //Ao se conectar deve ser usado a string 'mongodb://0.0.0.0:27017' no lugar de 'mongodb://localhost:27017' em alguns dipositivos
+    mongoose.connect('mongodb://0.0.0.0:27017').then(() => console.log('Conectado ao MongoDB')).catch((err) => console.log('Houve um erro ao se conectar ao MongoDB ' + err))
+    //Handlebars
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    //Public
+    app.use(express.static('./public'))
+//Rotas
+    //Home
+    app.get('/', (req, res) => {
+        res.render('index.handlebars')
+    })
+//Conexão
+const port = 2023
+app.listen(2023, () => console.log(`Servidor ativo na porta ${port}`))
