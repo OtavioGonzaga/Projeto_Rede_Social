d6115748388c01105a713f7fd969ae1e53ae1671
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-02-14 10:09:39 -0300

    Registro de usuários e verificação comentados

--- helpers/FormsValidation.js
+++ helpers/FormsValidation.js
@@ -1,8 +1,17 @@
-var errors = []
-function newAccountValidation(name, email, password) {
-    if (!name) errors.push('Insira um nome')
+function newAccountValidation(name, email, password, password2) { // Função de verificação de registro
+    let errors = [] // Array que contém as strings que informam os erros que o usuário cometeu au tentar se autenticar
+    if (!name || name === undefined || name === null) errors.push('Insira um nome')
     if (name.length <= 2) errors.push('Nome muito pequeno')
     if (name.length > 30) errors.push('Nome muito grande')
-    if (!email) errors.push('Insira um email')
-    
+    if (!email || email === undefined || email === null) errors.push('Insira um e-mail')
+    if (email.length < 8 || email.length > 250 || email.includes(' ') || !email.includes('@')) errors.push('Insira um e-mail válido')
+    if (password.length < 6) errors.push('Senha muito pequena')
+    if (password.length > 25) errors.push('Senha muito longa')
+    if (password.includes(' ')) errors.push('A senha não deve conter espaços')
+    if (password != password2) errors.push('Digite a mesma senha nos dois espaços')
+    return errors // Retorna os erros do cliente
+}
+// Exportações
+module.exports = {
+    newAccountValidation
 }
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const upload = require('../config/multer')
 const {hashPassword, comparePasswords} = require('../config/bcrypt.js') //Importa duas  funções do bcrypt
 const {isAuthenticated} = require('../helpers/AccessControl') //Importa uma verificação que só dá acesso a algumas rotas caso o usuário esteja autenticado
+const {newAccountValidation} = require('../helpers/FormsValidation') //COMENTA ISSO AQUI TAMBÉM
 const passport = require('passport')
 //Data
 var DataAtt = new Date()
@@ -54,36 +55,48 @@ const Users = mongoose.model('users')
         res.render('user/newuser')
     })
     router.post('/register', upload.single('profileimg'), async (req, res) => {
-            try {
+            try { //Tenta acessar o caminho da imagem upada pelo usuário na pasta uploads, caso não consiga, definirá que o caminho é 'uploads/default'.
                 var profileImgPath = req.file.path
             } catch (error) {
                 var profileImgPath = 'uploads/default.png'
             }
-            let newUser = {
+            let newUser = { // Acessa os inputs do html e salva os values em um objeto
                 name: req.body.name.trim(),
                 email: req.body.email.trim(),
-                password: await hashPassword(req.body.password.trim()),
+                password: req.body.password.trim(),
+                password2: req.body.password2.trim(),
                 profileimg: profileImgPath
             }
-            console.log(newUser.profileimg)
-            new Users(newUser).save().then(() => {
-                console.log('Usuário registrado com sucesso')
-                res.redirect('../')
-            }).catch((err) => {
-                console.error('Erro ao registrar usuário: \n' + err)
-                req.flash('error', 'Houve um erro interno ao registrar a conta')
-                res.redirect('../')
-            })
+            let verification = newAccountValidation(newUser.name, newUser.email, newUser.password, newUser.password2) // Função do arquivo FormsValidation.js da pasta helpers que faz a verificação dos values passados pelo usuário
+            if (verification.length === 0) { // Caso a verificação não retorne nenhum erro o processo de registro será continuado
+                delete newUser.password2 // Deleta a verificação de senha, pois ela não será selva no banco de dados
+                newUser.password = await hashPassword(newUser.password) // Espera o bcrypt gerar o hash da senha (pasta config)
+                new Users(newUser).save().then(() => { // Após toda a verificação, as informações do usuário são salvas no banco de dados.
+                    req.flash('success', 'Usuário registrado com sucesso')
+                    res.redirect('../')
+                }).catch((err) => { // Previne que a aplicação quebre ao registrar um erro.
+                    console.error('Erro ao registrar usuário: \n' + err)
+                    req.flash('error', 'Houve um erro interno ao registrar a conta')
+                    res.redirect('../')
+                })
+            } else { // Caso a verificação retorne algum erro irá alertar o usuário usando o flash
+                verification.map((e) => {
+                    e = ' ' + e
+                    req.flash('error', e)
+                })
+                res.redirect('./register')
+            }
+            
     })
     //Login
     router.get('/login', (req, res) => {
         res.render('user/login')
     })
     router.post('/login', (req, res, next) => {
-        passport.authenticate('local', {
-            successRedirect: '/',
-            failureRedirect: '/user/login',
-            failureFlash: true //Ativa as mensagens do connect-flash e exibe o texto passado através de um objeto {message: 'mensagem'} como argumento em um erro em config/auth.js
+        passport.authenticate('local', { // Informa o passport que a estratégia de autenticação é a local, em seguida passa um objeto com informações do que fazer após a tentativa de autenticação.
+            successRedirect: '/', // Em caso de sucesso redireciona o usuário para a home do site.
+            failureRedirect: '/user/login', //Em caso de falha redireciona para a página de login e exibe o erro.
+            failureFlash: true //Ativa as mensagens do connect-flash e exibe o texto passado através de um objeto {message: 'mensagem'} como argumento em um erro em config/auth.js. Para isso é necessário criar um middleware do connect-flash chamado 'error' (em variável global do node)
         })(req, res, next)
     })
 //Exportações
