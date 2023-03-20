e64200bea5b165927c8d297c316f8409cace7eff
Otavio Gonzaga <otaviolgonzaga@gmail.com>
2023-03-20 10:49:03 -0300

    Sistema de alteração de foto de perfil

--- config/multer.js
+++ config/multer.js
@@ -1,20 +1,20 @@
-const multer = require("multer")
+const multer = require("multer") // ATENÇÃO!!! É NECESSÁRIO QUE O FORMULÁRIO QUE USARÁ O MULTER TENHA A PROPIEDADE enctype="multipart/form-data"
 const path = require("path")
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/')
+    destination: (req, file, cb) => { //Define o destino do arquivo recebido
+        cb(null, 'uploads')
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname)) //Define o nome do arquivo com base no tempo em milisegundos e adiciona a extensão que nomeia o arquivo
     }
 })
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
-        cb(null, true);
+    if (file.mimetype.startsWith('image/')) { //Filtra o tipo de arquivo para aceitar apenas imagens
+        cb(null, true)
     } else {
         return 'Selecione uma imagem como foto de perfil'
     }
-  }
+}
 const upload = multer({
     storage: storage,
     fileFilter: fileFilter
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 //Carregando módulos
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const router = express.Router()
 //Config
 const imgHash = require('../config/imageToBase64') //Importa uma função do arquivo especificado que trasforma uma imagem em string
+const upload = require('../config/multer')
 //Helpers
 const {findUser} = require('../helpers/findSchema') //Importa uma função que busca os usuários, passando o email no primeiro argumento, e retorna usando o lean() ou não dependendo se o segundo argumento é true ou false (caso não seja passado será false)
+//Acessando bancos de dados
+require('../models/Users')
+const Users = mongoose.model('users')
 //Data
 var DataAtt = new Date()
 setInterval(() => {
@@ -17,8 +22,31 @@ router.get('/', async (req, res) => {
     res.render('user/user', {user})
 })
 //Foto de perfil (/profileimg)
-router.get('/profileimg', (req, res) => {
-    res.render('user/profileimg')
+router.get('/profileimg', async (req, res) => {
+    const user = await findUser(req.session.passport.user, true)
+    user.profileImg = await imgHash(user.profileImg)
+    res.render('user/profileimg', {user})
+})
+router.post('/profileimg', upload.single('profileimg'), (req, res) => {
+    if (!req.file) res.redirect('/user')
+    Users.findOne({email: req.session.passport.user}).then((user) => {
+        user.profileImg = req.file.path
+        user.save(err => {
+            if (err) {
+                console.log(err)
+                req.flash('error', 'Houve um erro ao alterar a foto de perfil')
+                res.redirect('/user')
+            } else {
+                // A antiga foto de perfil deve ser excluída na próxima versão
+                req.flash('success', 'Foto de perfil alterada com êxito')
+                res.redirect('/user')
+            }
+        })
+    }).catch((err) => {
+        console.log(err)
+        req.flash('error', 'Houve um erro ao alterar a foto de perfil')
+        res.redirect('/user')
+    })
 })
 //Edit (/edit)
 router.get('/edit', async (req, res) => {
